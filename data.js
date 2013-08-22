933e2d18f3c81575ed51efc155ff878264b3132e
Josh Wolfe <thejoshwolfe@gmail.com>
2013-08-21 21:08:36 -0700

    moving database to its own file

---
+++ data.js
@@ -0,0 +1,112 @@
+if (!window.Betrayal) window.Betrayal = {};
+
+window.Betrayal.characters = {
+  "Brandon Jaspers": {
+    colorClass: "green",
+    traits: [
+      {name: "Speed",  values: [0,3,4,4,4,5,6,7,8], start: 3},
+      {name: "Might",  values: [0,2,3,3,4,5,6,6,7], start: 4},
+      {name: "Sanity", values: [0,3,3,3,4,5,6,7,8], start: 4},
+      {name: "Knowl",  values: [0,1,3,3,5,5,6,6,7], start: 3},
+    ],
+  },
+  "Darrin \"Flash\" Williams": {
+    colorClass: "red",
+    traits: [
+      {name: "Speed",  values: [0,4,4,4,5,6,7,7,8], start: 5},
+      {name: "Might",  values: [0,2,3,3,4,5,6,6,7], start: 3},
+      {name: "Sanity", values: [0,1,2,3,4,5,5,5,7], start: 3},
+      {name: "Knowl",  values: [0,2,3,3,4,5,5,5,7], start: 3},
+    ],
+  },
+  "Father Rhinehardt": {
+    colorClass: "white",
+    traits: [
+      {name: "Speed",  values: [0,2,3,3,4,5,6,7,7], start: 3},
+      {name: "Might",  values: [0,1,2,2,4,4,5,5,7], start: 3},
+      {name: "Sanity", values: [0,3,4,5,5,6,7,7,8], start: 5},
+      {name: "Knowl",  values: [0,1,3,3,4,5,6,6,8], start: 4},
+    ],
+  },
+  "Heather Granville": {
+    colorClass: "purple",
+    traits: [
+      {name: "Speed",  values: [0,3,3,4,5,6,6,7,8], start: 3},
+      {name: "Might",  values: [0,3,3,3,4,5,6,7,8], start: 3},
+      {name: "Sanity", values: [0,3,3,3,4,5,6,6,6], start: 3},
+      {name: "Knowl",  values: [0,2,3,3,4,5,6,7,8], start: 5},
+    ],
+  },
+  "Jenny LeClerc": {
+    colorClass: "purple",
+    traits: [
+      {name: "Speed",  values: [0,2,3,4,4,4,5,6,8], start: 4},
+      {name: "Might",  values: [0,3,4,4,4,4,5,6,8], start: 3},
+      {name: "Sanity", values: [0,1,1,2,4,4,4,5,6], start: 5},
+      {name: "Knowl",  values: [0,2,3,3,4,4,5,6,8], start: 3},
+    ],
+  },
+  "Madame Zostra": {
+    colorClass: "blue",
+    traits: [
+      {name: "Speed",  values: [0,2,3,3,5,5,6,6,7], start: 3},
+      {name: "Might",  values: [0,2,3,3,4,5,5,5,6], start: 4},
+      {name: "Sanity", values: [0,4,4,4,5,6,7,8,8], start: 3},
+      {name: "Knowl",  values: [0,1,3,4,4,4,5,6,6], start: 4},
+    ],
+  },
+  "Missy Dubourde": {
+    colorClass: "yellow",
+    traits: [
+      {name: "Speed",  values: [0,3,4,5,6,6,6,7,7], start: 3},
+      {name: "Might",  values: [0,2,3,3,3,4,5,6,7], start: 4},
+      {name: "Sanity", values: [0,1,2,3,4,5,5,6,7], start: 3},
+      {name: "Knowl",  values: [0,2,3,4,4,5,6,6,6], start: 4},
+    ],
+  },
+  "Ox Bellows": {
+    colorClass: "red",
+    traits: [
+      {name: "Speed",  values: [0,2,2,2,3,4,5,5,6], start: 5},
+      {name: "Might",  values: [0,4,5,5,6,6,7,8,8], start: 3},
+      {name: "Sanity", values: [0,2,2,3,4,5,5,6,7], start: 3},
+      {name: "Knowl",  values: [0,2,2,3,3,5,5,6,6], start: 3},
+    ],
+  },
+  "Peter Akimoto": {
+    colorClass: "green",
+    traits: [
+      {name: "Speed",  values: [0,3,3,3,4,6,6,7,7], start: 4},
+      {name: "Might",  values: [0,2,3,3,4,5,5,6,8], start: 3},
+      {name: "Sanity", values: [0,3,4,4,4,5,6,6,7], start: 4},
+      {name: "Knowl",  values: [0,3,4,4,5,6,7,7,8], start: 3},
+    ],
+  },
+  "Professor Longfellow": {
+    colorClass: "white",
+    traits: [
+      {name: "Speed",  values: [0,2,2,4,4,5,5,6,6], start: 4},
+      {name: "Might",  values: [0,1,2,3,4,5,5,6,6], start: 3},
+      {name: "Sanity", values: [0,1,3,3,4,5,5,6,7], start: 3},
+      {name: "Knowl",  values: [0,4,5,5,5,5,6,7,8], start: 5},
+    ],
+  },
+  "Vivian Lopez": {
+    colorClass: "blue",
+    traits: [
+      {name: "Speed",  values: [0,3,4,4,4,4,6,7,8], start: 4},
+      {name: "Might",  values: [0,2,2,2,4,4,5,6,6], start: 3},
+      {name: "Sanity", values: [0,4,4,4,5,6,7,8,8], start: 3},
+      {name: "Knowl",  values: [0,4,5,5,5,5,6,6,7], start: 4},
+    ],
+  },
+  "Zoe Ingstrom": {
+    colorClass: "yellow",
+    traits: [
+      {name: "Speed",  values: [0,4,4,4,4,5,6,8,8], start: 4},
+      {name: "Might",  values: [0,2,2,3,3,4,4,6,7], start: 4},
+      {name: "Sanity", values: [0,3,4,5,5,6,6,7,8], start: 3},
+      {name: "Knowl",  values: [0,1,2,3,4,4,5,5,5], start: 3},
+    ],
+  },
+};
